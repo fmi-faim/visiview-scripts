1ad849b19d9a48ec9880f8045a2f5517dd5167d1
Laurent Gelman <laurent.gelman@fmi.ch>
2017-03-30 11:49:02 +0200

    no message

--- Miscellaneous/ClearPosList.py
+++ Miscellaneous/ClearPosList.py
@@ -1,2 +1,3 @@
 # VisiView Macro
+VV.Acquire.Stage.PositionList.Save("C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\LastList.stp")
 VV.Acquire.Stage.PositionList.Load("C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\EmptyList.stp")
---
+++ Miscellaneous/RestorePosList.py
@@ -0,0 +1,5 @@
+# VisiView Macro
+try:
+	VV.Acquire.Stage.PositionList.Load("C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\LastList.stp")
+except:
+	pass
