ae4814ca9f0e14c06811d618d4da4d68a9b667ed
Laurent Gelman <laurent.gelman@fmi.ch>
2017-03-31 09:12:20 +0200

    bug fixes

--- Miscellaneous/ClearPosList.py
+++ Miscellaneous/ClearPosList.py
@@ -1,3 +1,11 @@
 # VisiView Macro
-VV.Acquire.Stage.PositionList.Save("C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\LastList.stp")
-VV.Acquire.Stage.PositionList.Load("C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\EmptyList.stp")
+
+CancelButton = True
+try:
+	VV.Macro.MessageBox.ShowAndWait("Are you sure you want to delete all positions?", "Warning", CancelButton)
+	VV.Acquire.Stage.PositionList.Save("C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\LastList.stp")
+	VV.Acquire.Stage.PositionList.Load("C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\EmptyList.stp")
+except:
+	pass
+
+
