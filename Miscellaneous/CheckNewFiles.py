a5d69308c10766ac8c11628c74cd94f970dff39e
Laurent Gelman <laurent.gelman@fmi.ch>
2020-08-27 15:54:21 +0200

    New Dialog box to enter parameters

--- Miscellaneous/CheckNewFiles.py
+++ Miscellaneous/CheckNewFiles.py
@@ -1,30 +1,40 @@
 # VisiView Macro
-import sys
+import sys, os, glob
 sys.path.append(r"C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git")
 sys.path.append(r"C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\lib")
 sys.path.append(r"C:\ProgramData\Visitron Systems\VisiView\PythonMacros\FMI-git\faim-common-utils")
 vvimport('fileutils')
 import EmailToolbox
 VV.Macro.PrintWindow.Clear()
+VV.Macro.PrintWindow.IsVisible = True
 
 # *********** Parameters **********
 
 delay = 1 #minutes
-emailAdresse = "laurent.gelman@fmi.ch"
+emailAddress = "laurent.gelman@fmi.ch"
 
 # **********************************
 
+VV.Macro.InputDialog.Initialize("Set Parameters", True)
+VV.Macro.InputDialog.AddFloatVariable("Check frequency (in minutes):", "delay", 1, 1, 1000, 1)
+VV.Macro.InputDialog.AddStringVariable("E-mail address:", "emailAddress", "FirstName.LastName@fmi.ch")
+VV.Macro.InputDialog.AddDirectoryVariable("Select the directory", "directory", "")
+VV.Macro.InputDialog.Show()
+
 older_file = ""
-latest_file = "xx"
+list_of_files = glob.glob(os.path.join(directory,"*"))
+latest_file = max(list_of_files, key=os.path.getctime)
 
 while(latest_file != older_file):
 	older_file = latest_file
-	list_of_files = glob.glob(r'C:\temp\Nikolas\*')
+	#list_of_files = glob.glob(r'C:\temp\Nikolas\*')
+	list_of_files = glob.glob(os.path.join(directory,"*"))
 	latest_file = max(list_of_files, key=os.path.getctime)
-	print latest_file
+	print ("Latest File in the Folder: "+latest_file)
+	print ("Waiting "+str(int(delay))+" min...")
 	VV.Macro.Control.Delay(delay,'min')
 	
-print ("No new file since "+str(delay)+" min")
-InfoMail = EmailToolbox.Email(destin = emailAdresse, title = "Acquisition stopped", message = "No new file since "+str(delay)+" min")
+print ("No new file since "+str(int(delay))+" min")
+InfoMail = EmailToolbox.Email(destin = emailAddress, title = "Acquisition stopped", message = "No new file since "+str(delay)+" min")
 InfoMail.send()
 
